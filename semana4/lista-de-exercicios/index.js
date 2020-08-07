c4a55da9ad487001192e3382e4b60f0b06ec8202
yvinimayza <yvinimayza@gmail.com>
2020-08-07 14:22:44 -0300

    exercicio1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,8 @@
+// Exercicico 1 
+
+// Foi criado uma function conversorDeMoedas com o parametro valorEmDolar
+// dentro da função foi criado uma const cotacao que recebe a cotação atraves do prompt
+// e retona o valor em dolar vezes a cotação. Criaram uma const meuDinheiro que recebe  a função
+// com o parametro 100.e da um console.log no meu dinheiro...  O codigo tranforma o valor de dolar em reais.. 
+// A impressão no console vai depender do que o usuario digitar no prompt
+// Exemplo: se o usuario digitar 5 o console ira imprimir 500...
