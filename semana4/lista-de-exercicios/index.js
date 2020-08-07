9541823045575e11bd671528fb518817ea8e3215
yvinimayza <yvinimayza@gmail.com>
2020-08-07 14:35:34 -0300

    exercicio2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -6,3 +6,13 @@
 // com o parametro 100.e da um console.log no meu dinheiro...  O codigo tranforma o valor de dolar em reais.. 
 // A impressão no console vai depender do que o usuario digitar no prompt
 // Exemplo: se o usuario digitar 5 o console ira imprimir 500...
+
+// Execicio 2
+// A função investeDinheiro recebe dois parâmetros, o tipo de invetimento e o valor
+// e declara uma variável do valor após o investimento dentro da função.
+// Após, é criado uma condiciional que compara o tipo de investimento recebido com alguns cases (poupança, renda fixa, cdb, etc), para cada caso, existe uma operação aritimética do valor de investimento.
+// após esse switch é retornado o valor após o investimento.
+
+// Fora da função foi criada duas constantes que passam os parâmetros.
+// No primeiro console.log será exibido o valor após o investimento, que será 150 * 1.1, conforme o case dentro do Switch
+// e no segundo console caíra no default do switch que retornará "Tipo de investimento informado incorreto"
