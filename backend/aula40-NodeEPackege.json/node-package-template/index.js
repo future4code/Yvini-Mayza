fd9bf8b1aa87a2fcf5be16d3a56f2590d9bcd2f8
yvinimayza <yvinimayza@gmail.com>
2020-10-19 22:23:00 -0300

    alterações2

--- backend/aula40-NodeEPackege.json/node-package-template/index.js
+++ backend/aula40-NodeEPackege.json/node-package-template/index.js
@@ -7,42 +7,42 @@ const countries = require("./countries")
 
 // EXERCICIO 2
 
-const operation = process.argv[2];
-const number1 = Number(process.argv[3]);
-const number2 = Number(process.argv[4]);
+// const operation = process.argv[2];
+// const number1 = Number(process.argv[3]);
+// const number2 = Number(process.argv[4]);
 
-switch(operation){
-    case "sum":
-        console.log(number1+number2)
-    break
+// switch(operation){
+//     case "sum":
+//         console.log(number1+number2)
+//     break
 
-    case "sub":
-        console.log(number1-number2)
-    break
-    case "mult":
-        console.log(number1*number2)
-    break
+//     case "sub":
+//         console.log(number1-number2)
+//     break
+//     case "mult":
+//         console.log(number1*number2)
+//     break
 
-    case"div":
-        console.log(number1/number2)
-    break
+//     case"div":
+//         console.log(number1/number2)
+//     break
 
-    default:
-        console.log("Operação não encontrada")
-    break
+//     default:
+//         console.log("Operação não encontrada")
+//     break
 
-}
+// }
 
 // EXERCICIO 3
 
-// const task = process.argv[2]
-// const taskList =["Lavar a louça","Comprar Leite"]
+const task = process.argv[2]
+const taskList =["Lavar a louça","Comprar Leite"]
 
-// const addTask = (task) => {
-//     taskList.push(...taskList,task)
-//     return console.log(`Tarefa adicionada com sucesso! Tarefa:, ${taskList}`)
-// };
-// addTask(task)
+const addTask = (task) => {
+    taskList.push(...taskList,task)
+    return console.log(`Tarefa adicionada com sucesso! Tarefa:, ${taskList}`)
+};
+addTask(task)
 
 
 
